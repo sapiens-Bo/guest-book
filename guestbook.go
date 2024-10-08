0fd433958ac4083e398f18389e630f7516c3a275
Dorzh_Lenove-B50-45 <b4d.r@yandex.ru>
2024-10-08 19:53:19 +0300

    output data with templates

--- guestbook.go
+++ guestbook.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"text/template"
 )
+
+type GuestBook struct {
+	SignatureCount int
+	Signatures     []string
+}
 
 func check(err error) {
 	if err != nil {
@@ -12,10 +20,32 @@ func check(err error) {
 	}
 }
 
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
 func viewHandle(w http.ResponseWriter, r *http.Request) {
+	signatures := getStrings("signature.txt")
+	fmt.Printf("%#v\n", signatures)
 	html, err := template.ParseFiles("static/view.html")
 	check(err)
-	err = html.Execute(w, nil)
+	guestbook := GuestBook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(w, guestbook)
 	check(err)
 }
 
